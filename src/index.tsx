3e312240eafd0cc5ab98545dcb6e7ff658cabf42
Sanberk Zulfikar <sanberk@192.168.1.3>
2024-05-23 17:20:14 +0300

    deploy fix

--- src/index.tsx
+++ src/index.tsx
@@ -1,16 +1,17 @@
 import React from "react";
-import ReactDOM from "react-dom";
+import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App";
 import { QueryClient, QueryClientProvider } from "react-query";
 
 const queryClient = new QueryClient();
+const container = document.getElementById("root");
+const root = createRoot(container!); // Use the non-null assertion operator
 
-ReactDOM.render(
+root.render(
   <React.StrictMode>
     <QueryClientProvider client={queryClient}>
       <App />
     </QueryClientProvider>
-  </React.StrictMode>,
-  document.getElementById("root")
+  </React.StrictMode>
 );
